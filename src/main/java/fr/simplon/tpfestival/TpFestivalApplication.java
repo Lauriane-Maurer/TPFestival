c98d7345d4262a6a31ae93eb62148dd5b2c3040b
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-04-11 10:12:33 +0200

    Initial commit

---
+++ src/main/java/fr/simplon/tpfestival/TpFestivalApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.tpfestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFestivalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFestivalApplication.class, args);
+    }
+
+}
